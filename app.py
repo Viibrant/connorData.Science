4b872a825b67da920244a4e824d1aa9d189cd97e
Connor <connorbrook@googlemail.com>
2021-09-22 18:48:41 +0100

    added code for github webhooks

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from os import getcwd
+import git
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,6 +18,16 @@ def projects():
 @app.route("/contacts")
 def contacts():
     return render_template("contacts.html")
+
+@app.route("/update_server", mehods=["POST"])
+def webhook():
+    if request.method == "POST":
+        repo = git.Repo(getcwd())
+        origin = repo.remotes.origin
+        origin.pull()
+        return "Updated PythonAnywhere successfully", 200
+    else:
+        return "Wrong event type", 400
 
 if __name__ == "__main__":
     app.run(debug=True)
