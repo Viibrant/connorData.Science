95bda5fa7d56f65d3794bf4d895d33b20d34d1df
your boy connor <connorbrook@googlemail.com>
2021-12-12 08:28:37 +0000

    rename server to website

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.serving import run_simple
 
 from proj.covid import app as covid_app
-from server import app as server
+from website import app as server
 
 app = DispatcherMiddleware(server, {"/covid": covid_app})
 
